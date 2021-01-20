698202ed2dc7be3bc93b8e939e14318db1e13bd2
Manuel Reber <reberma@student.ethz.ch>
2021-01-20 20:06:53 +0100

    refactoring

--- copula.py
+++ copula.py
@@ -7,39 +7,38 @@ import numpy as np
 from statistics import NormalDist
 
 
-rv = np.random.uniform(0, 1, 10000)
-rho = 0.8
-type(rv)
+
+def get_copula_vectors(rv, rho=0.8):
+	X1 = rv[:,0]
+	X2 = rho * X1 + math.sqrt(1 - rho**2) * rv[:,1]
+	return X1, X2
 
 
 
-vecfunc = np.vectorize(NormalDist(mu=0, sigma=1).inv_cdf)
-norm_rv_array_1d = vecfunc(rv)
+if __name__ == '__main__':
 
-norm_rv_array_2d = np.reshape(norm_rv_array_1d, (-1, 2))
+	rv = np.random.uniform(0, 1, 10000)
 
-norm_rv_df = pd.DataFrame(norm_rv_array_2d, columns=['V1', 'V2'])
+	# without vectorize function just using list comprehension
+	norm_rv_df = np.asarray([NormalDist(mu=0, sigma=1).inv_cdf(i) for i in rv]).reshape(-1,2)
 
-
-
-# X1 = norm_rv_df.iloc[:,[0]]
-# X2 = rho * norm_rv_df.iloc[:,[0]] + (1 - rho**2)**0.5 * norm_rv_df.iloc[:,[1]]
-
-X1 = norm_rv_df['V1']
-X2 = rho * norm_rv_df['V1'] + math.sqrt(1 - rho**2) * norm_rv_df['V2']
-
-vectors = [X1, X2]
-
-result = pd.concat(vectors)
-
-print(result)
-
-np.cov(X1, X2)[0][1]
-np.var(X1)
+	# get X1 and X2
+	X1,X2 = get_copula_vectors(norm_rv_df)
 
 
+	#concat two numpy arrays
+	result= np.concatenate((X1, X2), axis = None)
 
-plt.plot(X1, X2, 'r')
+
+	print(result)
+
+
+	np.cov(X1, X2)[0][1]
+	np.var(X1)
+
+
+	plt.plot(X1, X2, 'r')
+	plt.show()
             
 
 
